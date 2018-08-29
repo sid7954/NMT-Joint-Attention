9c79e3354d380d1c4e6753ca43dcb6beb662cbd6
Siddhant Garg <sid7954@gmail.com>
2018-08-29 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tem/wmt16_en_de.sh to NMT Joint Attention

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for filename in ~/Desktop/tem/*.json; do
+for filename in ~/Desktop/tem/*.sh; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to NMT Joint Attention"
-    GIT_COMMITTER_DATE="Thu 30 Aug 2018 20:19:19 BST" git commit --amend --no-edit --date "Thu 30 Aug 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Wed 29 Aug 2018 20:19:19 BST" git commit --amend --no-edit --date "Wed 29 Aug 2018 20:19:19 BST"
 done
 git push origin master
