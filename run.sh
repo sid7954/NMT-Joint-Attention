7d7b8a3ab26b65f9a4776e5ab03b829475a0eeb8
Siddhant Garg <sid7954@gmail.com>
2018-09-03 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tem/iwslt15_joint1.json to NMT Joint Attention

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ for filename in ~/Desktop/tem/*.json; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to NMT Joint Attention"
-    GIT_COMMITTER_DATE="Tue 4 Sep 2018 20:19:19 BST" git commit --amend --no-edit --date "Tue 4 Sep 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Mon 3 Sep 2018 20:19:19 BST" git commit --amend --no-edit --date "Mon 3 Sep 2018 20:19:19 BST"
 done
 git push origin master
