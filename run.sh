f1bab155813ce38c7d22670ec9ca07ef46ecd0a2
Siddhant Garg <sid7954@gmail.com>
2018-09-07 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tem/iwsl15_luong_batch32.json to NMT Joint Attention

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for filename in ~/Desktop/tem/*.py; do
+for filename in ~/Desktop/tem/*.json; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to NMT Joint Attention"
-    GIT_COMMITTER_DATE="Sat 8 Sep 2018 20:19:19 BST" git commit --amend --no-edit --date "Sat 8 Sep 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Fri 7 Sep 2018 20:19:19 BST" git commit --amend --no-edit --date "Fri 7 Sep 2018 20:19:19 BST"
 done
 git push origin master
