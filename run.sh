b3b1535e534bab3318e1c7364b890f2c5ec86b80
Siddhant Garg <sid7954@gmail.com>
2018-09-15 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tem/evaluation_utils.py to NMT Joint Attention

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ for filename in ~/Desktop/tem/*.py; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to NMT Joint Attention"
-    GIT_COMMITTER_DATE="Mon 16 Sep 2018 20:19:19 BST" git commit --amend --no-edit --date "Mon 16 Sep 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Sun 15 Sep 2018 20:19:19 BST" git commit --amend --no-edit --date "Sun 15 Sep 2018 20:19:19 BST"
 done
 git push origin master
